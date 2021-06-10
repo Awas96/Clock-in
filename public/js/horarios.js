8a1daaebc1f4aee63e2cc9f64179324c06f1a32b
Awas <awas831996@gmail.com>
2021-06-10 14:16:35 +0200

    creacion de eventos con full calendar

--- public/js/horarios.js
+++ public/js/horarios.js
@@ -1,6 +1,7 @@
+var calendar;
 document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
+    calendar = new FullCalendar.Calendar(calendarEl, {
         headerToolbar: {
             left: 'today,prev,next',
             center: 'title',
@@ -12,12 +13,29 @@ document.addEventListener('DOMContentLoaded', function () {
         editable: true,
         dateClick: function (info) {
             pulsarDia(info)
-        }
+        },
+        events:[
+            {
+                title: 'Evento1',
+                descripcion: "awas was here",
+                start: '2021-06-10 10:00:00',
+            }
+        ]
     });
     calendar.render();
 
 });
 
 function pulsarDia(info) {
-    $("#exampleModal").modal();
+    $('#Modal').modal()
+    let titulo = document.querySelector("#modal-title");
+    titulo.innerText = "Evento de dia:  " +info.dateStr;
+
+    let  event = {id:1 , title: 'nuevoEvento', start: info.dateStr};
+    calendar('renderEvent', {
+        title: "Trabajo",
+        start: info.dateStr,
+        allday: true
+    });
+
 }
