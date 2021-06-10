9b9d2553625f094b73f616a4953308d178c11252
awas96 <awas831996@gmail.com>
2021-06-10 02:03:18 +0200

    añadido modal de fullcalendar

--- public/js/horarios.js
+++ public/js/horarios.js
@@ -2,14 +2,22 @@ document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
         headerToolbar: {
-            left:'today,prev,next',
-            center:'title',
-            right:'dayGridMonth,dayGridWeek,timeGridDay'
+            left: 'today,prev,next',
+            center: 'title',
+            right: 'dayGridMonth,dayGridWeek,timeGridDay'
         },
         locale: 'es',
         initialView: 'dayGridMonth',
         selectable: true,
         editable: true,
+        dateClick: function (info) {
+            pulsarDia(info)
+        }
     });
     calendar.render();
+
 });
+
+function pulsarDia(info) {
+    $("#exampleModal").modal();
+}
